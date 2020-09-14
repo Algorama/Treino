e81582719a0ae08c196986fd30cf78a799e19010
Rodrigo Sendin <rodrigo@algorama.com.br>
2020-09-14 09:46:31 -0300

    Aula: 013 - Classes e Objetos

---
+++ Exercicios/Exercicios.Domain/Cachorro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicios.Domain
+{
+    public class Cachorro
+    {
+        public string Latir()
+        {
+            return "Au! Au!";
+        }
+
+        public string QuantoDevoComer(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
---
+++ Exercicios/Exercicios.Tests/CachorroTest.cs
@@ -0,0 +1,41 @@
+﻿using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercicios.Tests
+{
+    [TestClass]
+    public class CachorroTest
+    {
+        [TestMethod]
+        public void Latir_Test()
+        {    
+            Cachorro leia = new Cachorro();
+            string latido = leia.Latir();
+
+            Console.WriteLine(latido);
+
+            Assert.AreEqual("Au! Au!", latido);
+        }
+
+        [TestMethod]
+        public void Comer_Test()
+        {
+            // Considerando que a Leia pesa 1Kg,
+            // e come 5% do seu peso de ração, 
+            // implemnte o Método "QuantoDevoComer" 
+            // para passar no Teste
+
+            Cachorro leia = new Cachorro();
+            string quantidadeDeRacao = leia.QuantoDevoComer(1);
+
+            Console.WriteLine(quantidadeDeRacao);
+
+            Assert.AreEqual("Como tenho 1Kg, devo comer 50g/dia", 
+                quantidadeDeRacao);
+        }
+    }
+}
+
+
+
