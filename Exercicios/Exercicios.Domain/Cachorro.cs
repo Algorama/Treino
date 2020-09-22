8ac05c1eee5ccae19683045925fe0478b788bccc
Rodrigo Sendin <rodrigo@algorama.com.br>
2020-09-22 09:48:30 -0300

    Aula: 014.1 - Métodos e Atributos - Solução dos Exercícios

--- Exercicios/Exercicios.Domain/Cachorro.cs
+++ Exercicios/Exercicios.Domain/Cachorro.cs
@@ -1,13 +1,88 @@
-﻿namespace Exercicios.Domain
+﻿using System;
+
+namespace Exercicios.Domain
 {
     public class Cachorro
     {
+        #region Nome
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+        public string GetNome()
+        {
+            return _nome;
+        }
         private string _nome;
+        #endregion
+
+        #region Sexo
+        public void SetSexo(string sexo)
+        {
+            _sexo = sexo;
+        }
+        public string GetSexo()
+        {
+            return _sexo;
+        }
         private string _sexo;
+        #endregion
+
+        #region Raça
+        public void SetRaca(string raca)
+        {
+            _raca = raca;
+        }
+        public string GetRaca()
+        {
+            return _raca;
+        }
         private string _raca;
+        #endregion
+
+        #region Porte
+        public void SetPorte(string porte)
+        {
+            _porte = porte;
+        }
+        public string GetPorte()
+        {
+            return _porte;
+        }
         private string _porte;
+        #endregion
+
+        #region Idade
+        public void SetIdade(int idade)
+        {
+            _idade = idade;
+        }
+
+        public int GetIdade()
+        {
+            return _idade;
+        }
         private int _idade;
+        #endregion
+
+        #region Peso
+        public void SetPeso(double peso)
+        {
+            if (peso < 0)
+            {
+                _peso = 0;
+            }
+            else
+            {
+                _peso = peso;
+            }
+        }
+        public double GetPeso()
+        {
+            return _peso;
+        }
         private double _peso;
+        #endregion
 
         public string Latir()
         {
@@ -18,16 +93,6 @@
         {
             // Calculando 5% do peso em Gramas
             return $"Como tenho {pesoKg}Kg, devo comer {pesoKg * 50}g/dia";
-        }
-
-        public void SetNome(string nome)
-        {
-            _nome = nome;
-        }
-
-        public string GetNome()
-        {
-            return _nome;
         }
     }
 }
--- Exercicios/Exercicios.Tests/CachorroTest.cs
+++ Exercicios/Exercicios.Tests/CachorroTest.cs
@@ -64,5 +64,77 @@ namespace Exercicios.Tests
             Console.WriteLine(nome);
             Assert.AreEqual("Yuri", nome);
         }
+
+        [TestMethod]
+        public void Cachorro_Set_Get_Sexo_Test()
+        {
+            Cachorro leia = new Cachorro();
+
+            leia.SetSexo("Fêmea");
+            string sexo = leia.GetSexo();
+
+            Console.WriteLine(sexo);
+            Assert.AreEqual("Fêmea", sexo);
+        }
+
+        [TestMethod]
+        public void Cachorro_Set_Get_Raca_Test()
+        {
+            Cachorro tequila = new Cachorro();
+
+            tequila.SetRaca("Labrador");
+            string raca = tequila.GetRaca();
+
+            Console.WriteLine(raca);
+            Assert.AreEqual("Labrador", raca);
+        }
+
+        [TestMethod]
+        public void Cachorro_Set_Get_Porte_Test()
+        {
+            Cachorro yuri = new Cachorro();
+
+            yuri.SetPorte("Médio");
+            string porte = yuri.GetPorte();
+
+            Console.WriteLine(porte);
+            Assert.AreEqual("Médio", porte);
+        }
+
+        [TestMethod]
+        public void Cachorro_Set_Get_Idade_Test()
+        {
+            Cachorro tequila = new Cachorro();
+
+            tequila.SetIdade(4);
+            int idade = tequila.GetIdade();
+
+            Console.WriteLine(idade);
+            Assert.AreEqual(4, idade);
+        }
+
+        [TestMethod]
+        public void Cachorro_Set_Get_Peso_Test()
+        {
+            Cachorro leia = new Cachorro();
+
+            leia.SetPeso(1.2);
+            double peso = leia.GetPeso();
+
+            Console.WriteLine(peso);
+            Assert.AreEqual(1.2, peso);
+        }
+
+        [TestMethod]
+        public void Cachorro_Peso_Nao_Pode_Ser_Negativo_Test()
+        {
+            Cachorro leia = new Cachorro();
+
+            leia.SetPeso(-1.2);
+            double peso = leia.GetPeso();
+
+            Console.WriteLine(peso);
+            Assert.AreEqual(0, peso);
+        }
     }
 }
