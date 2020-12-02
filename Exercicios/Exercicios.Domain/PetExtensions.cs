aa636467b0313644403740f9f05700d199157ca5
Rodrigo Sendin <rodrigo@algorama.com.br>
2020-12-02 15:45:10 -0300

    Aula: 030.1 - System.IO - Solução dos Exercícios

--- Exercicios/Exercicios.Domain/PetExtensions.cs
+++ Exercicios/Exercicios.Domain/PetExtensions.cs
@@ -1,10 +1,96 @@
-﻿namespace Exercicios.Domain
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exercicios.Domain
 {
     public static class PetExtensions
     {
         public static string GetTipo(this IPet pet)
         {
             return pet.GetType().Name;
+        }
+
+        public static void CarregaPetsDoArquivo(this List<IPet> pets, string caminho)
+        {
+            var donos = new List<Dono>();
+            var racas = new List<Raca>();
+
+            var linhas = File.ReadAllLines(caminho);
+            for (var i = 1; i < linhas.Length; i++)
+            {
+                var colunas = linhas[i].Split(";".ToCharArray());
+                if (colunas[0] == "Cachorro")
+                {
+                    var cachorro = new Cachorro();
+                    cachorro.SetPropriedadesComuns(donos, colunas);
+                    cachorro.Raca = racas.GetRaca(colunas[5], colunas[6]);
+                    cachorro.DataNascimento = Convert.ToDateTime(colunas[7]);
+                    cachorro.Vacinado = colunas[8] == "sim";
+                    pets.Add(cachorro);
+                }
+                else if (colunas[0] == "Gato")
+                {
+                    var gato = new Gato();
+                    gato.SetPropriedadesComuns(donos, colunas);
+                    pets.Add(gato);
+                }
+            }
+        }
+
+        private static void SetPropriedadesComuns(this IPet pet, List<Dono> donos, string[] colunas)
+        {
+            pet.Nome = colunas[1];
+            pet.Sexo = colunas[2] == "Macho" ? Sexo.Macho : Sexo.Femea;
+            pet.Peso = double.Parse(colunas[4]);
+
+            var dono = donos.GetDono(colunas[3]);
+            dono.AddPet(pet);
+        }
+
+        private static Dono GetDono(this List<Dono> donos, string donoNome)
+        {
+            foreach (var dono in donos)
+            {
+                if (dono.Nome == donoNome)
+                    return dono;
+            }
+
+            var novoDono = new Dono { Nome = donoNome };
+            donos.Add(novoDono);
+
+            return novoDono;
+        }
+
+        private static Raca GetRaca(this List<Raca> racas, string racaNome, string porte)
+        {
+            foreach (var raca in racas)
+            {
+                if (raca.Nome == racaNome)
+                    return raca;
+            }
+
+            var novaRaca = new Raca
+            {
+                Nome = racaNome,
+                Porte = GetPorte(porte)
+            };
+            racas.Add(novaRaca);
+
+            return novaRaca;
+        }
+
+        private static Porte GetPorte(string porte)
+        {
+            switch (porte)
+            {
+                case "Pequeno":
+                    return Porte.Pequeno;
+                case "Médio":
+                    return Porte.Medio;
+                default:
+                    return Porte.Grande;
+            }
         }
     }
 }
---
+++ Exercicios/Exercicios.Tests/PetTest.cs
@@ -0,0 +1,23 @@
+﻿using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Exercicios.Tests
+{
+    [TestClass]
+    public class PetTest
+    {
+        [TestMethod]
+        public void CarregaPetsDoArquivo_Test()
+        {
+            var pets = new List<IPet>();
+            pets.CarregaPetsDoArquivo("C:\\Aula030\\pets.csv");
+
+            Assert.AreEqual(15, pets.Count);
+
+            foreach(var pet in pets)
+                Console.WriteLine($"[{pet.GetTipo()}] {pet.Nome} - {pet.Dono.Nome}");
+        }
+    }
+}
